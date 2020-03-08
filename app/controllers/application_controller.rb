7da56de71603f7daf63fdcd6732ccd68ab4c2f50
AsanoSogen <AsanoSogen>
2020-03-08 22:01:06 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,33 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # protect_from_forgery with: :exception
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  def after_sign_in_path_for(resource)
-    case resource
-    when HostUser
-      host_user_path(id: resource.id)
-    when User
-      user_path(id: resource.id)     
-    end
-  end
+  # def after_sign_in_path_for(resource)
+  #   case resource
+  #   when HostUser
+  #     host_user_path(id: resource.id)
+  #   when User
+  #     user_path(id: resource.id)     
+  #   end
+  # end
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
 
-  def user_params
-    params.require(:users).permit(user_id)
-  end
+  # def user_params
+  #   params.require(:users).permit(user_id)
+  # end
 
-  def host_user_params
-    params.require(:host_users).permit(host_user_id)
-  end
+  # def host_user_params
+  #   params.require(:host_users).permit(host_user_id)
+  # end
 
-  def after_sign_out_path_for(resource_or_scope)
-    root_path # ←redirect先にしたいpathを自分で書く
-  end
+  # def after_sign_out_path_for(resource_or_scope)
+  #   root_path # ←redirect先にしたいpathを自分で書く
+  # end
 
 end
