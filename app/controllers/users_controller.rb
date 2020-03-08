10ca11051d68f34c34496c3b80a370192ccb5b4f
AsanoSogen <AsanoSogen>
2020-03-08 22:27:50 +0900

    fix2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    
+
   end
 
   # def new
@@ -14,12 +14,7 @@ class UsersController < ApplicationController
   #   params.require(:user).permit(:name, :email)
   # end
 
-  # def ensure_correct_user
-  #   if @current_user.id != params[:id].to_i
-  #     flash[:notice] = "権限がありません"
-  #     redirect_to "/posts/index"
-  #   end
-  # end
+
 
     
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
-
-  # devise_for :host_users, controllers: {
-  #   sessions:      'host_users/sessions',
-  #   passwords:     'host_users/passwords',
-  #   registrations: 'host_users/registrations'
-  # }         
+  devise_for :users
 
   root  "users#index"
   
   resources :users, only: [:index] 
 
-  # resources :host_users, only: [:index, :show, :new, :create, :edit, :update]
+  
 
   
 
