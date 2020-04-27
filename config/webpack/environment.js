f405ea66da8864388b75def059788b82989220d0
AsanoSogen <AsanoSogen>
2020-04-27 15:18:49 +0900

    environment fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,19 +4,19 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
-module.exports = environment
-
-
-const { environment } = require('@rails/webpacker');
-const webpack = require('webpack');
-
-environment.plugins.set(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
-  }),
-);
-
 module.exports = environment;
+
+
+// const { environment } = require('@rails/webpacker')
+// const webpack = require('webpack');
+
+// environment.plugins.set(
+//   'Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery',
+//     jquery: 'jquery',
+//   }),
+// );
+
+// module.exports = environment;
