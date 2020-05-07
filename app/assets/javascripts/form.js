559c0fe3680aa0b8fa6b8be933fe49017302b64b
AsanoSogen <AsanoSogen>
2020-05-07 12:23:57 +0900

    fix

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,6 +1,6 @@
-// $( function() {
-//   $(".form-control_date").datetimepicker({
-//     showOtherMonths: true,
-//     selectOtherMonths: true 
-//   });
-// });
+$( function() {
+  $(".form-control_date").datetimepicker({
+    showOtherMonths: true,
+    selectOtherMonths: true 
+  });
+});
