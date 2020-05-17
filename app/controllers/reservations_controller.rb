6cb8ef76b0a400f47c83f5249a921c8e2b5810b8
AsanoSogen <AsanoSogen>
2020-05-17 23:35:04 +0900

    reservation-function&view-fix

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,19 @@
 class ReservationsController < ApplicationController
 
+  before_action :set_book
+
   def create
-    @reservation = current_user.reservations.create(reservation_params)
+    @reservation = @book.reservations.create(reservation_params)
     redirect_to root_path notice:"予約が完了しました"
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).permit(:book_id)
+    params.permit(:book_id, :user_id).merge(user_id: current_user.id)
   end
 
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
 end
