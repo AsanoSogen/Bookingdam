436e4066d7ca3ef80df8d0c63de15c07b36c6eae
AsanoSogen <AsanoSogen>
2020-05-24 16:58:00 +0900

    create-vue-eservation-user

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,3 +1,15 @@
 class Api::V1::BooksController < ActionController::API
 
+  def show
+    @book = Book.find(params[:id])
+    @users = @book.reservation_users
+  end
+  
+
+  private
+
+  def book_params
+    params.permit(:title, :text, :image, :published_at, :end_day, :address, :latitude, :longitude).merge(user_id: current_user.id)
+  end
+    
 end
